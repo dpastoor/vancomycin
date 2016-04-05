6fb43f71e0f39ebbd818423cabfb9964dacb4c7f
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-05 18:08:27 -0400

    add some utility functions

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,27 @@ NULL
 is_numeric <- function(x) {
   typeof(x) %in% c("double", "integer") && !is.factor(x)
 }
+
+between <- function(value, .min, .max, include_min = TRUE, include_max = TRUE) {
+  if (include_min) {
+    if (value < .min) {
+      return(FALSE)
+    }
+  } else {
+    if (value <= .min) {
+      return(FALSE)
+    }
+  }
+
+  if (include_max) {
+    if (value > .max) {
+      return(FALSE)
+    }
+  } else {
+    if (value >= .max) {
+      return(FALSE)
+    }
+  }
+
+  return(TRUE)
+}
