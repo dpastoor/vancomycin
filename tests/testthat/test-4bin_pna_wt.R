ffe4c31d4927748147b042b3f7225c15fcc21988
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-31 10:36:11 -0400

    update tests to more modern dialect

--- tests/testthat/test-4bin_pna_wt.R
+++ tests/testthat/test-4bin_pna_wt.R
@@ -2,11 +2,11 @@ context("4bin_pna_wt")
 
 starting_df <- data.frame(ID=1:4, WT = c(1.1, 1.1, 1.9, 1.9), AGE=c(6, 8, 6, 8))
 
-result_df <- by_row(starting_df, function(row) {
-  dosing_4bin_pna_wt(row$AGE,row$WT)
-}, .collate="rows")
 
 test_that("algorithm works with table test", {
+  result_df <- starting_df %>%
+    mutate(regimen = map2(AGE, WT, dosing_4bin_pna_wt)) %>%
+    unnest()
   expect_equal(result_df$DOSE, c(16.5, 16.5, 28.5, 28.5))
   expect_equal(result_df$II, c(24, 12, 18, 8))
 })
